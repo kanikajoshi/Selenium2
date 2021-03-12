6109a4f85ee932a749a395027f0109217998fbd7
kanika <anikadeoshi@gmail.com>
2021-03-11 21:02:20 -0500

    add

--- JavaPrograms/src/JavaProgams/TestInheritance2.java
+++ JavaPrograms/src/JavaProgams/TestInheritance2.java
@@ -4,12 +4,12 @@ public class TestInheritance2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BabyDog d=new BabyDog();  
+	/*	BabyDog d=new BabyDog();  
 		d.weep();  
 		d.bark();  
-		d.eat();  
+		d.eat();  */
 		}
 
 	}
 
-}
+
